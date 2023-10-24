b0a7b4b4420a0f0096bb0174779a6999ac555a26
OriShalhon <ori.shalhon@gmail.com>
2023-10-24 22:07:54 +0300

    raise states to app components, add redux toolkit and styled components for later implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import SideBar from './components/SideBar';
 
 function App() {
-  return (
-         <div className="App">
-            <SideBar />
-        </div>
-  );
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+    const [projects, setProjects] = useState<string[]>(['ProjectA',
+                                                                                                   'ProjectB',
+                                                                                                   'ProjectC']);
+
+    function toggleDarkMode() {
+        setDarkMode(!darkMode);
+    }
+
+    function addProject(newProject: string) {
+        setProjects([...projects, newProject]);
+    }
+
+    return (
+             <div className="App">
+                <SideBar isDarkMode ={darkMode}
+                         onChangeDarkMode = {() => toggleDarkMode()}
+                         projects = {projects}
+                         onAddProject = {(project:string) => addProject(project)}/>
+            </div>
+      );
 }
 
 export default App;
