14ec95d9d91174b01cc3ba221f10cab145f03b15
OriShalhon <ori.shalhon@gmail.com>
2023-10-29 20:31:28 +0200

    format prettier code - no logic change

--- src/components/CentralComponent.tsx
+++ src/components/CentralComponent.tsx
@@ -1,20 +1,20 @@
-import React, { useState } from 'react';
-import './CentralComponent.css'
-import ProjectTasks from './ProjectTasks';
-import {ProjectTasksProps} from './ProjectTasks';
+import React, { useState } from "react";
+import "./CentralComponent.css";
+import ProjectTasks from "./ProjectTasks";
+import { ProjectTasksProps } from "./ProjectTasks";
 
-const CentralComponent: React.FC<{ projects: ProjectTasksProps[]}> = ({ projects }) => {
-    return (
-        <div className='centralComponent'>
-            {projects
-            .filter(project => project.isVisibile)
-            .map((project) => (
-                <ProjectTasks key={project.id} projectData={project}  />
-            ))}
-        </div>
-    );
+const CentralComponent: React.FC<{ projects: ProjectTasksProps[] }> = ({
+  projects,
+}) => {
+  return (
+    <div className="centralComponent">
+      {projects
+        .filter((project) => project.isVisibile)
+        .map((project) => (
+          <ProjectTasks key={project.id} projectData={project} />
+        ))}
+    </div>
+  );
 };
 
 export default CentralComponent;
-
-
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
-import * as FaIcons from 'react-icons/fa';
-import './Header.css';
+import React, { useState } from "react";
+import * as FaIcons from "react-icons/fa";
+import "./Header.css";
 
 interface HeaderProps {
-    onToggleSideBar: () => void;
+  onToggleSideBar: () => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ onToggleSideBar }) => {
-    return (
-        <header>
-            <div className='header menu-padding'>
-                <a className='menu-bars' onClick={onToggleSideBar}>
-                    <FaIcons.FaBars />
-                </a>
-                <div className='header-buttons'>
-                    <button className='login-button'>Login</button>
-                    <button className='logout-button'>Logout</button>
-                    <div className='user-info'>User Info</div>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header>
+      <div className="header menu-padding">
+        <a className="menu-bars" onClick={onToggleSideBar}>
+          <FaIcons.FaBars />
+        </a>
+        <div className="header-buttons">
+          <button className="login-button">Login</button>
+          <button className="logout-button">Logout</button>
+          <div className="user-info">User Info</div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
