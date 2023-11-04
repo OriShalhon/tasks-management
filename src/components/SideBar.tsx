af289d477d39714fe1d5f8bacbf77f2b761dc916
OriShalhon <ori.shalhon@gmail.com>
2023-11-04 23:04:05 +0200

    some ui changes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -34,43 +34,42 @@ const Sidebar: React.FC<Props> = ({
 
   return (
     <>
-      <IconContext.Provider value={{ color: "#696969" }}>
-        <div
-          className={
-            isSidebarVisible ? "sidebar-content visible" : "sidebar-content"
-          }
-        >
-          <div className="sidebar-item">
-            <input
-              type="text"
-              placeholder="Enter project name"
-              value={newProject}
-              onChange={(e) => setNewProject(e.target.value)}
-            />
-            <button className="button" onClick={onAddProject}>
-              <FaIcons.FaPlus />
-            </button>
-          </div>
-          <ul>
-            {projects.map((project) => (
-              <li
-                className={
-                  project.isVisibile ? "sidebar-item selected" : "sidebar-item"
-                }
-                key={project.id}
-                onClick={() => dispatch(toggleProjectVisibility(project.id))}
-              >
-                {project.projectName}
-              </li>
-            ))}
-          </ul>
-          <DarkModeSwitch
-            onChange={() => dispatch(toggleDarkMode())}
-            checked={isDarkMode}
-            size={20}
+      <div
+        className={
+          isSidebarVisible ? "sidebar-content visible" : "sidebar-content"
+        }
+      >
+        <div className="sidebar-item">
+          <input
+            type="text"
+            placeholder="Enter project name"
+            value={newProject}
+            onChange={(e) => setNewProject(e.target.value)}
           />
+          <button className="button" onClick={onAddProject}>
+            <FaIcons.FaPlus />
+          </button>
         </div>
-      </IconContext.Provider>
+        <ul>
+          {projects.map((project) => (
+            <li
+              className={
+                project.isVisibile ? "sidebar-item selected" : "sidebar-item"
+              }
+              key={project.id}
+              onClick={() => dispatch(toggleProjectVisibility(project.id))}
+            >
+              {project.projectName}
+            </li>
+          ))}
+        </ul>
+        <DarkModeSwitch
+          className="dark-mode-switch"
+          onChange={() => dispatch(toggleDarkMode())}
+          checked={isDarkMode}
+          size={24}
+        />
+      </div>
     </>
   );
 };
