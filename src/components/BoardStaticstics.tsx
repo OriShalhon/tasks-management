832563291f6e6f6b5f52672b260779b2b6069253
OriShalhon <ori.shalhon@gmail.com>
2023-12-08 16:47:01 +0200

    include board statistics

---
+++ src/components/BoardStaticstics.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+import { ProjectTasksProps, TaskStatus } from '../store/slices/projectTasksSlice';
+
+interface Props {
+    projects: ProjectTasksProps[];
+}
+
+const BoardStatistics: React.FC<Props> = ({ projects }) => {
+    const getTaskAmountInStatus = (status: TaskStatus) => {
+        let amount = 0;
+        projects.forEach((project) => {
+            project.tasks.forEach((task) => {
+                if (task.status === status) amount++;
+            });
+        });
+        return amount;
+    };
+
+    const doneTasks = getTaskAmountInStatus(TaskStatus.done);
+    const newTasks = getTaskAmountInStatus(TaskStatus.new);
+    const inProgressTasks = getTaskAmountInStatus(TaskStatus.inProgress);
+
+    const finishedPercentage = Math.round(
+        (doneTasks / (doneTasks + newTasks + inProgressTasks)) * 100
+    );
+
+    return (
+        <div>
+            <CircularProgressbar value={finishedPercentage} text={`${finishedPercentage}%`} />;
+        </div>
+    );
+};
+
+export default BoardStatistics;
