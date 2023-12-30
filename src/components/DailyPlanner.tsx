4d241eed7e07f29ef5c6aa34a24954cf516ba466
orishalhon <ori.shalhon@gmail.com>
2023-12-30 11:36:50 +0200

    multiple developments

---
+++ src/components/DailyPlanner.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './DailyPlanner.css';
+const DailyPlanner: React.FC = () => {
+    return (
+        <div className = 'DailyPlanner'>
+            <h1>Daily planner</h1>
+        </div>
+    );
+};
+
+export default DailyPlanner;
